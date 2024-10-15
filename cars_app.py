7f47b3d08c01ad513b6ddc5a2389d5f08088512c
Braganca-rj <erik-bragancacarneiro28@hotmail.com>
2024-10-15 17:46:58 +0200

    git pull

--- cars_app.py
+++ cars_app.py
@@ -4,16 +4,29 @@ import plotly.express as px
 
 car_data = pd.read_csv('vehicles.csv')
 
-build_hist = st.checkbox('Create histogram')
-build_dispersion = st.checkbox('Create dispersion')
+st.header("Does it influence the price of your vehicle or not?")
+st.write("Let's discover the factors that influence the price of your vehicle when selling it by analyzing some online ads data.")
 
 
-if build_hist:
+# Groupings used to calculate the table values.
+odometer_price = car_data.groupby(
+    'odometer')['price'].value_counts().reset_index()
+
+year_price = car_data.groupby('model_year')[
+    'price'].value_counts().reset_index()
+
+condition_price = car_data.groupby(
+    'condition')['price'].value_counts().reset_index()
+
+fuel_price = car_data.groupby('fuel')['price'].value_counts().reset_index()
+
+type_price = car_data.groupby('type')['price'].value_counts().reset_index()
+
+# The First table(Odometer x Price)
+build_hist = st.write(
+    "Discover how much your car's mileage influences its sale.")
+build_dispersion_odometer = st.button('Create dispersion')
+
+if build_dispersion_odometer:
     st.write('Creating a histogram for the car sales ads dataset')
-    fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
-
-if build_dispersion:
-    st.write('Creating a histogram for the car sales ads dataset')
-    fig = px.scatter(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
+    px.scatter(odometer_price, y='price', x='odometer', color='odometer')
