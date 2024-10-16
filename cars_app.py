c309eab9d3a69facb9b4dd43b99c9d84e3f92c4e
Braganca-rj <erik-bragancacarneiro28@hotmail.com>
2024-10-16 16:49:35 +0200

    git pull

--- cars_app.py
+++ cars_app.py
@@ -26,10 +26,57 @@ fuel_price = car_data.groupby('fuel')['price'].value_counts().reset_index()
 type_price = car_data.groupby('type')['price'].value_counts().reset_index()
 
 # The First table(Odometer x Price)
-build_hist = st.write(
-    "Discover how much your car's mileage influences its sale.")
-build_dispersion_odometer = st.button('Create dispersion')
+title_odometer = st.title('Odometer x Price')
+build_odometer = st.write(
+    "Discover how much your car's mileage influences when it comes to selling.")
+build_dispersion_odometer = st.button('See the chart here of Odometer x Price')
 
 if build_dispersion_odometer:
-    st.write('Creating a histogram for the car sales ads dataset')
+    st.write('Creating a scatter graph for the car sales ads dataset')
     st.scatter_chart(odometer_price, y='price', x='odometer', color='odometer')
+
+# The Second table (Year x Price)
+title_year = st.title('Year x Price')
+build_year = st.write(
+    "Discover how much your car's year influences when it comes to selling.")
+build_bar_year = st.button('See the chart here of Year x Price')
+
+if build_bar_year:
+    st.write('Creating a bar graph for the car sales ads dataset')
+    st.bar_chart(year_price, x='model_year', y='price', color='price')
+
+# The third table (Condition x Price)
+
+title_condition = st.title('Condition x Price')
+build_condition = st.write(
+    "Discover how much a car's condition influences when it comes to selling it.")
+build_scatter_condition = st.button('See the chart  of Condition x Price')
+
+
+if build_scatter_condition:
+    st.write('Creating a scatter graph for the car sales ads dataset')
+    st.scatter_chart(condition_price, y='condition', x='price', color='price')
+
+# The fourth table (Fuel x Price)
+
+title_fuel = st.title('Fuel x Price')
+build_fuel = st.write(
+    "Discover how much a car's fuel consumption influences the time to sell it.")
+build_bar_fuel = st.button('See the chart here of Fuel x Price')
+
+
+if build_bar_fuel:
+    st.write('Creating a bar graph for the car sales ads dataset')
+    st.bar_chart(fuel_price, x='fuel', y='price', color='fuel')
+
+# The fifth table (Type x Price)
+
+title_type = st.title("Type of cars x Price")
+build_type = st.write(
+    'Discover how much the type of car influences the price when selling it')
+build_scatter_type = st.button('See the chart here of Type x Price')
+
+
+if build_scatter_type:
+    st.write('Creating a scatter graph for the car sales ads dataset')
+    st.scatter_chart(type_price, y='type', x='price', color='type')
